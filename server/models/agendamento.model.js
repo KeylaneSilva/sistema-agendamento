08f4769a427497ec5139b8a1a22ec1747e924f8c
Keylane <keylanessilva07@gmail.com>
2022-01-29 22:37:37 -0300

    model de agendamento

--- server/models/agendamento.model.js
+++ server/models/agendamento.model.js
@@ -5,9 +5,10 @@ const DataSchema = new mongoose.Schema({
     valor_procedimento: Number,
     data_agendamento: {
         type: Date,
-        default: Date.new()
+        default: Date.now()
     },
     horario_agendamento: String,
+    //ref cliente
     cliente:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "clientes",
