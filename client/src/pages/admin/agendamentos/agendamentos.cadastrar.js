8bb7ebc875bfa416d93eed96c84c4d8968720bf0
Keylane <keylanessilva07@gmail.com>
2022-02-10 10:31:30 -0300

    paginas de agendamentos

---
+++ client/src/pages/admin/agendamentos/agendamentos.cadastrar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AgendamentosCadastrar() {
+  return (
+    <div>
+        <h1>Agendamentos cadastrar</h1>
+    </div>
+  )
+}
---
+++ client/src/pages/admin/agendamentos/agendamentos.editar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AgendamentosEditar() {
+  return (
+    <div>
+        <h1>Agendamentos editar</h1>
+    </div>
+  )
+}
