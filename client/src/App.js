58fecd36dbdbcee5ad761d4690bfe464b6a1eb75
Keylane <keylanessilva07@gmail.com>
2022-02-10 02:29:07 -0300

    client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Album from './pages/client/painel'
+
+export default function App() {
+  return (
+    <div>
+      {/* <h1>Sistema de agendamento</h1> */}
+      <Album />
+    </div>
+  )
+}
